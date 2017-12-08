d97541f1eeef7266f6b99e824f157bbea9bf523f
xuejinhua <15896069641@126.com>
2017-12-08 14:41:22 +0800

    微信登录

--- src/main/java/com/demo/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/demo/service/impl/UserInfoServiceImpl.java
@@ -20,19 +20,19 @@ public class UserInfoServiceImpl implements UserInfoService {
      * 微信公众平台 设置中的开发设置中可以查到，相当于qq的qq号
      * 建议单独创建一个配置文件储存此类值和函数
      */
-    public static final String APP_ID = "wx0262c100d96ed074";
+    private static final String APP_ID = "wx0262c100d96ed074";
     /**
      * AppSecret 小程序密钥
      * 微信公众平台 设置中的开发设置中可以查询 但是每次查询会重置密钥 相当于qq的qq密码
      * 建议单独储存
      */
-    public static final String SECRET = "940d22a4afdf6dc0c211fa4cc81bdea6";
+    private static final String SECRET = "940d22a4afdf6dc0c211fa4cc81bdea6";
     /**
      * 用于获取用户openid的接口网址
      * 其中%s将会用String.format函数替换为实际的值
      * 建议单独储存
      */
-    public static final String WEB_ACCESS_TOKEN_HTTPS = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
+    private static final String WEB_ACCESS_TOKEN_HTTPS = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
 
     @Autowired
     private UserInfoRepository userInfoRepository;
